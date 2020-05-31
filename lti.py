948b0bc4854b9f30121210688e2e22fe8d68f5de
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-05-31 13:33:03 +0530

    Update lti.py

--- lti.py
+++ lti.py
@@ -1,5 +1,10 @@
 import pyshark
+from greynoise import GreyNoise
+api_client = GreyNoise(api_key=UW3D9zMw61AxTsy9ydprpLz6WGwZLFIJNJR1ra0bPVaKE37zsl3kOoPiiXfqfQUG)
 cap =pyshark.LiveCapture(interface='eth0')
 cap.sniff(timeout=1)
 for packet in cap.sniff_continuously():
 	print('Just arrived:', packet.ip.src)
+	data_of_ip=api_client.quick(packet)
+	print(data_of_ip)
+	
